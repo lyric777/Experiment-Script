565b811d5a3f89158ac67077148e2231accbef75
lyric777 <37208917+lyric777@users.noreply.github.com>
2019-07-10 19:39:15 +0800

    重返前20的不做计算

--- NAME/figure3d.py
+++ NAME/figure3d.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 28 19:56:40 2019
+Created on Mon Jun 17 14:37:12 2019
 
-@author: lenovo
+@author: xzl
 """
 
 import xlrd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
-from mpl_toolkits.mplot3d import Axes3D
 
-raw_data = xlrd.open_workbook(r'文化传递-名字.xlsx').sheet_by_name('female')
+
+raw_data = xlrd.open_workbook(r'文化传递-名字.xlsx').sheet_by_name('male')
 
 all_name = raw_data.col_values(0)
 all_location = raw_data.col_values(1)
@@ -20,7 +20,7 @@ all_year = raw_data.col_values(3)
 
 data = np.array([all_name,all_location,all_rank,all_year])
 
-name = r'静'
+name = r'建国'
 #liansheng = np.where(data[0] == r'连生')
 liansheng = np.where(data[0] == name)
 
@@ -46,8 +46,20 @@ for i in range(len(location)):
     for j in range(len(loc_axix)):
         if location[i] == loc_axix[j]:
             city_weight[j][year[i]-time_axix[0]]=weight[i]
-           
- 
+
+'''消失之后重新返回前20的不计算在内'''
+for city in city_weight:
+    for i in range(len(city)):
+        if city[i] > 0:
+            first = i;
+            break
+    for i in range(first,len(city)):
+        if city[i] == 0:
+            first_disappear = i;
+            break
+    for i in range(first_disappear,len(city)):
+        city[i] = 0;
+  
 color = ['aquamarine','bisque','black','blanchedalmond','blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dodgerblue','gainsboro','gold','gray','green','greenyellow','hotpink','indianred','khaki','lavender','lightsteelblue','magenta','mediumaquamarine','navajowhite','navy''oldlace','orange','palegoldenrod','pink','purple','red','springgreen','steelblue','tomato','turquoise','violet','whitesmoke','yellow','yellowgreen']
 #解决中文显示问题
 plt.rcParams['font.sans-serif']=['SimHei']
@@ -55,23 +67,22 @@ plt.rcParams['axes.unicode_minus'] = False
 plt.figure(figsize=(20, 8))
 
 plt.title('各地区每年名字“'+name+'”的权重变化')
-ax=plt.subplot(111, projection='3d')
+
 for i in range(len(loc_axix)):
     if loc_axix[i] == "北京市":
-        ax.plot(time_axix, [i for k in range(len(time_axix))], city_weight[i], color='#000000', linestyle='--', label=loc_axix[i])
+        plt.plot(time_axix, city_weight[i], color='#000000', linestyle='--', label=loc_axix[i])
     elif loc_axix[i]== "上海市":
-        ax.plot(time_axix, [i for k in range(len(time_axix))], city_weight[i], color='#000000', linestyle=':', label=loc_axix[i])
+        plt.plot(time_axix, city_weight[i], color='#000000', linestyle=':', label=loc_axix[i])
     elif loc_axix[i] == "广东省":
-        ax.plot(time_axix, [i for k in range(len(time_axix))], city_weight[i], color='#000000', linestyle='-.', label=loc_axix[i])
+        plt.plot(time_axix, city_weight[i], color='#000000', linestyle='-.', label=loc_axix[i])
     else:             
-        ax.plot(time_axix, [i for k in range(len(time_axix))], city_weight[i], color=color[i], label=loc_axix[i])
+        plt.plot(time_axix, city_weight[i], color=color[i], label=loc_axix[i])
 
-plt.legend(loc='upper left') # 显示图例
-'''
+plt.legend() # 显示图例
+plt.xlabel('年份')
+plt.ylabel('权重')
+ax = plt.axes() 
 ax.xaxis.set_major_locator(MultipleLocator(2))
 ax.yaxis.set_major_locator(MultipleLocator(2))
-ax.zaxis.set_major_locator(MultipleLocator(2))
-'''
-#plt.zlim(0,21)#把y轴的刻度范围设置为1到20
+plt.ylim(0,21)#把y轴的刻度范围设置为1到20
 plt.show()
-
